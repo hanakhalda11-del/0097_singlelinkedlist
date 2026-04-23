306fc5e6a0c758f618bda027acd01da409255cb0
hanakhalda11-del <hanakhalda11@gmail.com>
2026-04-23 19:37:55 +0700

    Refactor single linked list implementation

--- singlelinkedlist.cpp
+++ singlelinkedlist.cpp
@@ -56,3 +56,57 @@ public:
         previous->next = nodeBaru;
     }
 
+    bool listEmpty()
+    {
+        return (START == NULL);
+    }
+
+    bool search(int nim, Node *&previous, Node *&current)
+    {
+        previous = NULL;
+        current = START;
+
+        while (current != NULL && current->noMhs != nim)
+        {
+            previous = current;
+            current = current->next;
+        }
+
+        return (current != NULL);
+    }
+
+    bool delNode(int nim)
+    {
+        Node *current, *previous;
+
+        if (!search(nim, previous, current))
+            return false;
+
+        if (current == START)
+            START = START->next;
+        else
+            previous->next = current->next;
+
+        delete current;
+        return true;
+    }
+
+    void traverse()
+    {
+        if (listEmpty())
+        {
+            cout << "\nList kosong\n";
+            return;
+        }
+
+        cout << "\nData dalam list:\n";
+        Node *current = START;
+
+        while (current != NULL)
+        {
+            cout << current->noMhs << endl;
+            current = current->next;
+        }
+    }
+};
+
